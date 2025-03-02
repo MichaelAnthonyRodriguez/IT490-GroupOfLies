1e9b382d00e3d17310e5129e38b85974d6863f60
MichaelAnthonyRodriguez <156932206+MichaelAnthonyRodriguez@users.noreply.github.com>
2025-03-02 16:39:02 -0500

    c

--- app/consumer.php
+++ app/consumer.php
@@ -1,3 +1,27 @@
 <?php
-#scripts for consuming messages
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+$queue_name = 'task_queue';
+$channel->queue_declare($queue_name, false, true, false, false);
+
+echo " [*] Waiting for messages. To exit, press CTRL+C\n";
+
+$callback = function ($msg) {
+    echo " [x] Received ", $msg->body, "\n";
+    sleep(1); // Simulating processing time
+    echo " [x] Done\n";
+};
+
+$channel->basic_consume($queue_name, '', false, true, false, false, $callback);
+
+while ($channel->is_consuming()) {
+    $channel->wait();
+}
+
+$channel->close();
+$connection->close();
 ?>

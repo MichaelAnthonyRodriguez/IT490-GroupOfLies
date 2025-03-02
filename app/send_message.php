bf77aba3bd6a01d0328895e626ae4ff55a2d1d92
MichaelAnthonyRodriguez <156932206+MichaelAnthonyRodriguez@users.noreply.github.com>
2025-03-02 16:46:20 -0500

    s

--- app/send_message.php
+++ app/send_message.php
@@ -35,3 +35,44 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "Invalid request.";
 }
 ?>
+<html>
+    <head>
+        <title>Higher or Lower</title>
+        <link rel="stylesheet" href="app/static/style.css"/>
+    </head>
+    <body>
+        <!-- header -->
+        <header>
+            <img id="logo" src="images/logo.png"><h3>Higher or Lower</h3>
+            <nav class="menu">
+                <a href="index.php">Home</a>
+                <a href="register.php">Register</a>
+                <?php 
+                    if (isset($_SESSION['is_valid_admin'])==false){
+                        session_start();
+                    }
+                    if (isset($_SESSION['is_valid_admin'])) { 
+                ?>
+                
+                <a href="highOrLow.php">Play</a>
+                <a href="logout.php">Logout</a>
+                <p><a>
+                    <?php
+                        // require_once('userData.php');
+                        // userData();
+                    ?>
+                </a></p>
+                <?php } else { ?>
+                <a href="login.php">Login</a>
+                <?php } ?>              
+            </nav>
+        </header>
+        <!-- main elements -->
+        <main>
+        </main>
+        <hr>
+        <hr>
+        <footer></footer>
+        <hr>
+    </body>
+</html>

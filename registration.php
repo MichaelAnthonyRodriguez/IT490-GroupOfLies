53664a737608f5060abaaaec5c4fbb42b097155f
mikorod622 <mr94@njit.edu>
2025-03-05 02:51:45 -0500

    Update registration.php

--- registration.php
+++ registration.php
@@ -50,7 +50,7 @@ try {
 
     // Sends the request via RabbitMQ
     $client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
-    $response = $client->send_request($request);
+    $response = $client->publish($request);
 
     echo "Response from server:\n";
     print_r($response);

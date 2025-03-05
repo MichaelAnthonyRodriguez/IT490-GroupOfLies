5aa2d168af9b38cc71a2c658d752b5058be42fee
mikorod622 <mr94@njit.edu>
2025-03-04 20:11:13 -0500

    Update send_message.php

--- send_message.php
+++ send_message.php
@@ -56,3 +56,44 @@ try {
     echo "Error sending message: " . $e->getMessage();
 }
 ?>
+<html>
+    <head>
+        <title>Higher or Lower</title>
+        <link rel="stylesheet" href="app/static/style.css"/>
+    </head>
+    <body>
+        <!-- header -->
+        <header>
+            <img id="logo" src="images/logo.png">
+            <h3>Higher or Lower</h3>
+            <nav class="menu">
+                <a href="index.php">Home</a>
+                <a href="register.php">Register</a>
+                <?php 
+                    session_start();
+                    if (isset($_SESSION['is_valid_admin'])) { 
+                ?>
+                    <a href="highOrLow.php">Play</a>
+                    <a href="logout.php">Logout</a>
+                    <p><a>
+                        <?php
+                            // require_once('userData.php');
+                            // userData();
+                        ?>
+                    </a></p>
+                <?php } else { ?>
+                    <a href="login.php">Login</a>
+                <?php } ?>              
+            </nav>
+        </header>
+        <!-- main elements -->
+        <main>
+            <p>Welcome</p>
+        </main>
+        <hr>
+        <hr>
+        <footer></footer>
+        <hr>
+    </body>
+</html>
+

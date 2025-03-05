dc1299adfabb33d09a8206e7731616844255729e
mikorod622 <mr94@njit.edu>
2025-03-05 05:59:04 -0500

    Update loginRequest.php

--- loginRequest.php
+++ loginRequest.php
@@ -53,3 +53,35 @@ try {
   echo "Error sending message: " . $e->getMessage();
 }
 ?>
+<html>
+    <head>
+        <title>Higher or Lower</title>
+        <link rel="stylesheet" href="app/static/style.css"/>
+    </head>
+    <body>
+        <!-- header -->
+        <header>
+            <img id="logo" src="images/logo.png">
+            <h3>Higher or Lower</h3>
+            <nav class="menu">
+                <a href="index.php">Home</a>
+
+                <?php if (isset($_SESSION['is_valid_admin']) && $_SESSION['is_valid_admin'] === true) { ?>
+                    <a href="highOrLow.php">Play</a>
+                    <a href="logout.php">Logout</a>
+                    <p>Welcome, <strong><?php echo htmlspecialchars($_SESSION['first_name'] . " " . $_SESSION['last_name']); ?></strong>!</p>
+                    <?php } else { ?>
+                    <a href="register.php">Register</a>
+                    <a href="login.php">Login</a>
+                <?php } ?>
+            </nav>
+        </header>
+
+        <!-- main elements -->
+        <main>
+        </main>
+        
+        <hr>
+        <footer></footer>
+    </body>
+</html>

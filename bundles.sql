5e296c729c0ac9214633c14c013c49666993d283
Xinzoh <89925188+Xinzoh@users.noreply.github.com>
2025-04-08 13:19:37 -0400

    Delete bundles.sql

--- bundles.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE bundles (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,      -- e.g., "login"
-    version INT NOT NULL,             -- e.g., 1
-    status ENUM('new', 'passed', 'failed') DEFAULT 'new',
-    size BIGINT,                      -- Bundle size in bytes
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
